3d4b510a094fbe0d969c297d3527e73e395116fd
BZemba87 <zembabernadette@gmail.com>
2022-11-06 15:28:57 +0000

    add function Sidebar

---
+++ my-app/src/components/Sidebar.js
@@ -0,0 +1,13 @@
+
+function Sidebar() {
+    return (
+        <div>
+            <a href = "#." target= "_blank">My Photos</a>
+            <a href = "#." target= "_blank">My Illustrations</a>
+            <a href = "#." target= "_blank">My Paintings</a>
+        </div>    
+    )
+
+}
+
+export default Sidebar
