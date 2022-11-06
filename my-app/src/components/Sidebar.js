148d8b13e901920fc3663f8133c8316942d428bb
BZemba87 <zembabernadette@gmail.com>
2022-11-06 15:37:37 +0000

    add css to Sidebar component

--- my-app/src/components/Sidebar.js
+++ my-app/src/components/Sidebar.js
@@ -1,7 +1,8 @@
+import css from "./css/Sidebar.module.css"
 
 function Sidebar() {
     return (
-        <div>
+        <div className = {css.sidebar}>
             <a href = "#." target= "_blank">My Photos</a>
             <a href = "#." target= "_blank">My Illustrations</a>
             <a href = "#." target= "_blank">My Paintings</a>
